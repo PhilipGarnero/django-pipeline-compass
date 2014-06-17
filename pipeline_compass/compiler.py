7434737788561f895f50ee7e53da3c19dc18ba7f
PhilipGarnero <philip.garnero@gmail.com>
2014-06-17 04:50:00 +0200

    now working with config.rb

--- pipeline_compass/compiler.py
+++ pipeline_compass/compiler.py
@@ -2,6 +2,7 @@ from pipeline.compilers import CompilerBase
 from django.utils.encoding import smart_str
 from django.conf import settings
 
+from datetime import datetime
 import scss
 import os
 
@@ -13,36 +14,22 @@ def add_to_scss_path(path):
         scss.config.LOAD_PATHS = ','.join(load_paths) # reconstruct saved paths
 
 def get_attr_from_file(filename, attr):
-    """Get the value of an attribute within a file."""
-
     with open(filename) as fd:
         for line in fd:
             if line.startswith(attr):
                 return line[:-1].split('=')[1].strip(' ').strip("'").strip('"')
-    raise AttributeError("Couldn't find the attribute {0} in the file {1}".format(attr, filename))
+    return ''
 
 
 def init_compiler():
     if "done" not in init_compiler.__dict__:
         global css_path
-        try:
-            css_path = os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'css_dir'))
-        except:
-            css_path = ''
-        try:
-            add_to_scss_path(os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'sass_dir')))
-        except:
-            pass
-        try:
-            images_path = os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'images_dir'))
-        except:
-            images_path = ''
-        try:
-            generated_images_path = os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'generated_images_dir'))
-        except:
-            generated_images_path = ''
-        scss.config.STATIC_ROOT = images_dir if images_dir else os.path.join(compass_project_path, "images")
-        scss.config.ASSETS_ROOT = generated_images_dir if generated_images_dir else scss.config.STATIC_ROOT
+        css_path = os.path.abspath(os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'css_dir')))
+        add_to_scss_path(os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'sass_dir')))
+        images_path = os.path.abspath(os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'images_dir')))
+        generated_images_path = os.path.abspath(os.path.join(compass_project_path, get_attr_from_file(settings.PIPELINE_COMPASS_CONFIG_RB, 'generated_images_dir')))
+        scss.config.STATIC_ROOT = images_path if images_path != compass_project_path else os.path.join(compass_project_path, "images")
+        scss.config.ASSETS_ROOT = generated_images_path if generated_images_path != compass_project_path else scss.config.STATIC_ROOT
         init_compiler.done = True
 
 
@@ -50,12 +37,17 @@ class CompassCompiler(CompilerBase):
     output_extension = 'css'
 
     def match_file(self, filename):
+        init_compiler()
         return filename.endswith(('.scss', '.sass'))
 
+    def output_path(self, filename):
+        if css_path != compass_project_path:
+            return os.path.join(css_path, '.'.join((os.path.splitext(os.path.basename(filename))[0], self.output_extension)))
+        else:
+            filename = os.path.splitext(filename)
+            return '.'.join((filename[0], self.output_extension))
+
     def compile_file(self, infile, outfile, outdated=False, force=False):
-        init_compiler()
-        if css_path:
-            outfile = os.path.join(css_path, os.path.basename(outfile))
         add_to_scss_path(os.path.dirname(infile))
         if force or outdated:
             self.save_file(outfile, scss.Scss(scss_opts={
@@ -65,6 +57,10 @@ class CompassCompiler(CompilerBase):
 
     def save_file(self, path, content):
         return open(path, 'w').write(smart_str(content))
+
+    def is_outdated(self, infile, outfile):
+        return datetime.fromtimestamp(os.path.getmtime(infile)) > datetime.fromtimestamp(os.path.getmtime(outfile))
+
 
 compass_project_path = os.path.abspath(os.path.join(settings.PIPELINE_COMPASS_CONFIG_RB, os.pardir)) if settings.PIPELINE_COMPASS_CONFIG_RB else scss.config.PROJECT_ROOT
 # add compass builtins to scss load path
